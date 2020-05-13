e6af45abf9e7bc5f81271315e0b890a9d5cdfa95
dustin <yanshuochu@qq.com>
2020-05-13 17:02:58 -0500

    add fix missing to apt get

--- .github/workflows/install-MiKTeX_hithesis.sh
+++ .github/workflows/install-MiKTeX_hithesis.sh
@@ -8,7 +8,7 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
 echo "deb http://miktex.org/download/ubuntu bionic universe" | sudo tee /etc/apt/sources.list.d/miktex.list
 sudo apt-get update
-sudo apt-get install miktex
+sudo apt-get install miktex --fix-missing
 sudo miktexsetup --shared=yes finish
 sudo initexmf --admin --set-config-value [MPM]AutoInstall=1
 echo "Finish install MiKTeX."
